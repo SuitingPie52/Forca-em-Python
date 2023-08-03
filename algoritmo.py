0babfc0d3812d52edb9c8d08a58725303bbb03a3
SuitingPie52 <126472855+SuitingPie52@users.noreply.github.com>
2023-08-03 09:26:04 -0300

    Add files via upload

--- algoritmo.py
+++ algoritmo.py
@@ -1,13 +1,17 @@
 import random
 
-cores = ["branco", "azul", "vermelho", "amarelo", "verde"]
+cores = ["branco", "azul", "vermelho", "amarelo", "verde", "marrom", "bege", "roxo", "rosa", "cinza", "dourado",
+         "preto"]
 
 item_aleatorio = random.choice(cores)
 palavra_escondida = '*' * len(item_aleatorio)
 erros = 0
 i = 0
 
-while erros < 4 and palavra_escondida != item_aleatorio:
+print("------ BEM VINDO AO JOGO DA FORCA !!! ------\n\nEscolha a quantidade de erros que você pode cometer:")
+total_de_erros = int(input(str))
+
+while erros < total_de_erros + 1 and palavra_escondida != item_aleatorio:
 
     print("Forca:", palavra_escondida, "\n\nTente adivinhar uma letra:")
     tentativa = input(str)
@@ -30,9 +34,18 @@ while erros < 4 and palavra_escondida != item_aleatorio:
 
         palavra_escondida = nova_palavra_escondida
 
-        print("Você acertou!!!")
+        print("Tentativa correta !!!")
 
     else:
 
         erros += 1
         print("Errrrrrrouuuuuuu!!!!!!!!\nQuantidade de erros: ", erros)
+
+
+if erros > total_de_erros:
+
+    print("\nFim de jogo... Você ultrapassou o seu limite de erros.\nA palavra era:", item_aleatorio)
+
+elif palavra_escondida == item_aleatorio:
+
+    print("\nVocê venceu !!! Parabéns :D\nA palavra era:", palavra_escondida)
